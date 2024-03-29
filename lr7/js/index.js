7af32b5ca237cf2e185f0055a3330830b66c76f9
pipistrellla <xexexexaxaxa22848@gmail.com>
2024-03-29 17:16:24 +0500

    lr7
    доделал лр 7

--- lr7/js/index.js
+++ lr7/js/index.js
@@ -1,11 +1,71 @@
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-
-let userNumber = document.querySelector('.userNumber');
+// случайное число
+let randomNumber = randomNumberGenerator();
+// переменные которые выводим
 let lastUserNumber = document.querySelector('.lastUserNumber');
-let lowOrHi = document.querySelector('.lowOrHi');
-
-let guessSubmit = document.querySelector('.guessSubmit');
+let lowOrHi = document.querySelector('.lowOrHigh');
+let guessNumber = document.querySelector('.guessNumber');
+// кнопка и инпут в форме
 let guessField = document.querySelector('.guessField');
+let guessSubmit = document.getElementById('guessSubmit');
 
+// для работы функций
+let userNumber
 let guessCount = 1;
-let resetButton;
+
+function checkGuess(event) {
+    event.preventDefault();
+    userNumber = +guessField.value
+
+
+    if ((userNumber !== randomNumber) && (+guessCount < 5)){
+        guessCount+=1
+
+        lowOrHi.textContent = (userNumber > randomNumber) ? 
+            'Попробуй меньше' :
+            'Попробуй больше'
+
+        lastUserNumber.textContent = userNumber
+
+        guessNumber.textContent = guessCount
+        
+    } else {
+        gameOver(userNumber)
+    }
+    guessField.value = "";
+
+}
+
+function gameOver(userNumber) {
+    guessField.disabled = true;
+
+    lowOrHi.textContent = (userNumber === randomNumber) ? 'Ура победа' : 'эх поражение'
+    lastUserNumber.textContent = userNumber
+    guessNumber.textContent = guessCount
+    guessSubmit.textContent  = 'Начать игру сначала'
+    guessSubmit.removeEventListener("click", checkGuess)
+    guessSubmit.addEventListener("click", startAgain)
+}
+
+function startAgain(event) {
+    event.preventDefault();
+
+    guessField.disabled = false;
+    guessSubmit.textContent  = 'Испытать удачу'
+    randomNumber = randomNumberGenerator()
+    guessSubmit.removeEventListener("click", startAgain)
+    guessSubmit.addEventListener("click", checkGuess);
+    lowOrHi.textContent = ''
+    lastUserNumber.textContent = ''
+    guessNumber.textContent = ''
+    guessCount = 0
+}
+
+
+
+
+function randomNumberGenerator() {
+    return Math.floor(Math.random() * 10) +1;
+}
+
+
+guessSubmit.addEventListener("click", checkGuess);
